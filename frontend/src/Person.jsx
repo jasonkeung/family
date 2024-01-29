5b445246c1bb239ef478567c872e0a26edb1f2c4
Jason Keung <jason.keungg@gmail.com>
2024-01-28 23:57:12 -0800

    Add parent references

--- frontend/src/Person.jsx
+++ frontend/src/Person.jsx
@@ -1,17 +1,23 @@
 import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
+import { useContext } from 'react';
+import { PeopleContext } from './App';
 export const FIELDS = ["fullName"];
 
 
 export function Person(props) {
+    const {peopleMap} = useContext(PeopleContext)
+    const fields = props.fields;
+    const mother = peopleMap[fields?.mother?.referenceValue]
+    const father = peopleMap[fields?.father?.referenceValue]
 
     return (
         <Card variant="outlined">
             <CardContent>
-                <Typography level="title-md">{props.fields.name?.stringValue}</Typography>
-                <Typography>{props.fields.mother?.stringValue}</Typography>
-                <Typography>{props.fields.father?.stringValue}</Typography>
+                <Typography level="title-md">{fields?.fullName?.stringValue}</Typography>
+                <Typography>Mother: {mother?.fields.fullName.stringValue}</Typography>
+                <Typography>Father: {father?.fields.fullName.stringValue}</Typography>
             </CardContent>
         </Card>
     )
